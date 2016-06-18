38a4d41d221b843f15960c71a146a6d9c3474b6c
The Illustrious Andrew Theobald <andrew.t@matomy.com>
2016-06-17 21:40:40 -0400

    updated looping

--- pumpkinpi.py
+++ pumpkinpi.py
@@ -10,30 +10,30 @@ logging.basicConfig(filename='pumpkinpi.log', level=logging.DEBUG, format='%(asc
 logging.getLogger().addHandler(logging.StreamHandler())
 logging.info('script has been loaded waiting to start loop')
 
+cam = picamera.PiCamera()
+
 
 def start_timelapse(numba):
+
     logging.info('Starting timelapse')
-    with picamera.PiCamera() as cam:
-        counter = 1
 
-        while counter < 10:
-            cam.start_preview()
-            time.sleep(1)
+    cam.start_preview()
+    time.sleep(1)
 
-            imageName = ('%s.jpg' % time.strftime('%H:%M:%S-%m-%d-%Y'))
+    imageName = ('%s.jpg' % time.strftime('%H:%M:%S-%m-%d-%Y'))
 
-            cam.capture(imageName)
-            cam.stop_preview()
+    cam.capture(imageName)
+    cam.stop_preview()
 
-            try:
-                logging.info('Uploading %s to server' % imageName)
-                uploadtoserver.upload_picture(imageName)
-                logging.info('Success uploading to server...Posting %s to server' % imageName)
-                postpicture.post_image_to_server(imageName, numba)
-                logging.info('Success posting to site')
-            except Exception, e:
-                logging.error('Error uploading to services %s' % e.message)
-                emailerror.send_mail(e.message)
+    try:
+        logging.info('Uploading %s to server' % imageName)
+        uploadtoserver.upload_picture(imageName)
+        logging.info('Success uploading to server...Posting %s to server' % imageName)
+        postpicture.post_image_to_server(imageName, numba)
+        logging.info('Success posting to site')
+    except Exception, e:
+        logging.error('Error uploading to services %s' % e.message)
+        emailerror.send_mail(e.message)
 
 
 schedule.every().day.at("08:00").do(start_timelapse, 1)
@@ -43,4 +43,4 @@ schedule.every().day.at("17:00").do(start_timelapse, 4)
 
 while 1:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(60)
