0036c20c7b37fd35cfa6c1142822c2b949c212b0
The Illustrious Andrew Theobald <andrew.t@matomy.com>
2016-06-18 10:00:32 -0400

    schedule

--- pumpkinpi.py
+++ pumpkinpi.py
@@ -19,28 +19,32 @@ def start_timelapse(numba):
 
     cam.start_preview()
     time.sleep(1)
+    index = 0
+    while index < 50:
+        imageName = ('%s.jpg' % time.strftime('%H:%M:%S-%m-%d-%Y'))
 
-    imageName = ('%s.jpg' % time.strftime('%H:%M:%S-%m-%d-%Y'))
+        cam.capture(imageName)
+        cam.stop_preview()
 
-    cam.capture(imageName)
-    cam.stop_preview()
+        try:
+            logging.info('Uploading %s to server' % imageName)
+            uploadtoserver.upload_picture(imageName)
+            logging.info('Success uploading to server...Posting %s to server' % imageName)
+            postpicture.post_image_to_server(imageName, numba)
+            logging.info('Success posting to site')
+        except Exception, e:
+            logging.error('Error uploading to services %s' % e.message)
+            emailerror.send_mail(e.message)
 
-    try:
-        logging.info('Uploading %s to server' % imageName)
-        uploadtoserver.upload_picture(imageName)
-        logging.info('Success uploading to server...Posting %s to server' % imageName)
-        postpicture.post_image_to_server(imageName, numba)
-        logging.info('Success posting to site')
-    except Exception, e:
-        logging.error('Error uploading to services %s' % e.message)
-        emailerror.send_mail(e.message)
+        index += 1
+        time.sleep(300)
 
 
-schedule.every().day.at("08:00").do(start_timelapse, 1)
-schedule.every().day.at("11:00").do(start_timelapse, 2)
-schedule.every().day.at("14:00").do(start_timelapse, 3)
-schedule.every().day.at("17:00").do(start_timelapse, 4)
-
-while 1:
-    schedule.run_pending()
-    time.sleep(300)
+# schedule.every().day.at("08:00").do(start_timelapse, 1)
+# schedule.every().day.at("11:00").do(start_timelapse, 2)
+# schedule.every().day.at("14:00").do(start_timelapse, 3)
+# schedule.every().day.at("17:00").do(start_timelapse, 4)
+#
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(300)
