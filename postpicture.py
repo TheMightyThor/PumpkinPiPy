fb581fec15c77de048c78c61de50a96cef7f5c4a
The Illustrious Andrew Theobald <andrew.t@matomy.com>
2016-06-17 20:13:13 -0400

    test

--- postpicture.py
+++ postpicture.py
@@ -7,6 +7,6 @@ xyz = PAPER_PLATE['noggin']
 def post_image_to_server(fileName):
     logging.info('posting to website')
 
-    r = requests.post(PAPER_PLATE['commandcenter'] + '/imageData?name=s%' % fileName, headers={ 'xyz' : xyz})
+    r = requests.post(PAPER_PLATE['commandcenter'] + '/imageData?name=%s' % fileName, headers={ 'xyz' : xyz})
 
     logging.info('posted file to site response = %s' % r.text)
