7e3f30d3af8ffedea7b22db4443f5ab86076c426
Andrew Theobald <andrewtheobald@Andrews-MacBook-Pro.local>
2017-09-09 19:27:33 -0400

    allnight

--- emailerror.py
+++ emailerror.py
@@ -6,7 +6,7 @@ from email.MIMEText import MIMEText
 from pumpkinpiconfig import PAPER_PLATE
 
 FROM = PAPER_PLATE['specialsauce']
-TO = PAPER_PLATE['theman']
+TO = PAPER_PLATE['theman','yanetgarridocoutin@gmail.com']
 SUBJECT = 'Error in Pi'
 SERVER = 'smtp.mail.yahoo.com'
 PORT = 587
@@ -22,6 +22,29 @@ def send_mail(error):
     msg.attach(text)
 
     print('connecting to smtp server')
+    try:
+        s = smtplib.SMTP(SERVER, PORT)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(FROM, PAPER_PLATE['ketchup'])
+        s.sendmail(FROM, TO, msg.as_string())
+        s.quit()
+        logging.info('sent mail!')
+    except Exception, ex:
+        logging.error('something bad happened error message = %s' % str(ex))
+
+def send_mail_with_video(fileName):
+    msg = MIMEMultipart()
+    msg['From'] = 'SoFloPiInTheSky'
+    msg['Subject'] = SUBJECT
+    msg['To'] = TO
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(fileName, "rb").read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename='fileName'')
+    msg.attach(part)
     try:
         s = smtplib.SMTP(SERVER, PORT)
         s.ehlo()
